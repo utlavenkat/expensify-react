aba3614d039c2918a06eb5d675056825a368307c
Venkat Utla <venkatasubbaiah.utla@adp.com>
2020-10-03 13:43:53 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './styles/styles.scss'
+
+
+ReactDOM.render(<p>Initial Commit</p>, document.getElementById('app'))
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader','css-loader','sass-loader']
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
